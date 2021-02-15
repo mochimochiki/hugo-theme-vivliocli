18837a9dae08c5735bbaf3f4361301a66f1aa01a
mochimo <mochikimoto@yahoo.co.jp>
2021-02-16 00:39:34 +0900

    build.batでPDF出力まで

--- exampleSite/CI/build.bat
+++ exampleSite/CI/build.bat
@@ -24,18 +24,10 @@ hugo --environment %hugo_env%
 if not %errorlevel% == 0 exit /B 1
 
 :: ----------
-:: JP Help
+:: JP PDF
 :: ----------
-:: convert to sjis
-:: pushd %scriptdir%
-:: powershell -NoProfile -ExecutionPolicy RemoteSigned ^
-::   ".\UTF8toLocalCode.ps1 -dir \"%hugodir%\public_%hugo_env%\jp\" -codename shift_jis -custom_table utf8_to_shift_jis.csv -logname unknown_%hugo_env%.log;exit $LASTEXITCODE"
-:: if not %errorlevel% == 0 exit /B 1
-:: popd
-
-:: hhc.exe public_%hugo_env%\jp\project.hhp
-:: hhc.exe returns 1 on success  https://ja.coder.work/so/cmd/1267049
-:: if not %errorlevel% == 1 exit /B 1
+vivliostyle build -c public_%hugo_env%\jp\index.js
+if not %errorlevel% == 1 exit /B 1
 
 
 popd
