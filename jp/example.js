390960c2bcce718df7f1d5adeea37d5730620c06
mochimochiki <mochimochiki@users.noreply.github.com>
2021-02-27 05:50:27 +0000

    deploy: 0d1dd34b64f61c4d60261bc2423619ea4055a5a2

---
+++ jp/example.js
@@ -0,0 +1,26 @@
+module.exports = {
+title: 'Hugo-theme-vivliocli ガイド',
+size: 'A4',
+entry: [
+'example.cover.html',
+'../jp/GettingStarted.html',
+'../jp/GettingStarted/GettingStarted.html',
+'../jp/Configuration.html',
+'../jp/Configuration/config.html',
+'../jp/Configuration/pdfconfig.html',
+'../jp/Configuration/shortcodes.html',
+'../jp/Configuration/frontmatter.html',
+'../jp/Configuration/MarkdownShowcase.html',
+'../jp/SamplePart.html',
+'../jp/SamplePart/SampleChapter1.html',
+'../jp/SamplePart/SampleChapter1/sample1.html',
+'../jp/SamplePart/SampleChapter1/sample2.html',
+'../jp/SamplePart/SampleChapter2.html',
+'../jp/SamplePart/SampleChapter2/sample1.html',
+'../jp/SamplePart/SampleChapter2/sample2.html',
+'example.colophon.html'
+],
+output: [
+'Hugo-theme-vivliocli ガイド.pdf'
+]
+}
