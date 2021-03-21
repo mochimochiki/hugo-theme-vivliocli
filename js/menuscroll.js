9fc15dd74e95d76ca4fbf78815af963a0c1657a8
mochimochiki <mochimochiki@users.noreply.github.com>
2021-03-21 12:24:30 +0000

    deploy: 070b8c71704e5aeb5153752cfe326493ee5dcd92

---
+++ js/menuscroll.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", function () {
+  var menuElement = document.getElementById('menu');
+  var selectedElement = document.getElementById('menu-selected');
+  var yOffset = selectedElement.getBoundingClientRect().top;
+  var position = yOffset - (window.innerHeight / 2)
+  menuElement.scrollTo(0, position);
+}, false);
