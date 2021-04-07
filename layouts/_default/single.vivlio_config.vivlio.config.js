a1595592d0d80dbd018388667bd836aaeeab8891
mochimo <mochikimoto@yahoo.co.jp>
2021-04-08 00:17:15 +0900

    fix: custom output format でpdfconfigを出力可能にした #20

---
+++ layouts/_default/single.vivlio_config.vivlio.config.js
@@ -0,0 +1,28 @@
+{{- $pdfname := partial "functions/outputEditionParam" (dict "page" . "param" "pdfname" ) -}}
+{{- $doctitle := partial "functions/outputEditionParam" (dict "page" . "param" "doctitle" ) -}}
+module.exports = {
+  title: '{{ $doctitle }}',
+  {{ if .Params.author }}author: '{{ .Params.author }}',{{ end }}
+  size: '{{ .Params.pagesize }}',
+  entry: [
+    '_pdfcover.vivlio.cover.html',
+    {{- define "entry_hierarchy" }}
+      {{- range .Pages.ByWeight }}
+        {{- if partial "functions/isShow" . }}
+          {{- $url := urls.Parse (.RelPermalink | urlize) }}
+          {{- $path := $url.Path }}
+          {{- $link := printf "..%s" $path }}
+    '{{ $link }}',
+          {{- if .IsSection }}{{ template "entry_hierarchy" . }}{{ end }}
+        {{- end }}
+      {{- end }}
+    {{- end }}
+    {{ template "entry_hierarchy" .CurrentSection }}
+    {{- if .Params.colophon }}
+    '_pdfcolophon.vivlio.colophon.html'
+    {{- end }}
+  ],
+  output: [
+    '{{ printf "%s.pdf" $pdfname }}'
+  ]
+}
