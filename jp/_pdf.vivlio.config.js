09ab70942fa7fdda0e1103189745cab85f918a8c
mochimochiki <mochimochiki@users.noreply.github.com>
2021-04-08 12:07:26 +0000

    deploy: 3b12643fd2c6d7808da61ed27832fb4bcfdad789

---
+++ jp/_pdf.vivlio.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  title: 'Hugo-theme-vivliocli Guide',
+  author: 'mochimo',
+  size: 'A4',
+  entry: [
+    '_pdf.vivlio.cover.html',
+    
+    '../jp/GettingStarted.html',
+    '../jp/GettingStarted/GettingStarted.html',
+    '../jp/Configuration.html',
+    '../jp/Configuration/config.html',
+    '../jp/Configuration/pdfconfig.html',
+    '../jp/Configuration/shortcodes.html',
+    '../jp/Configuration/frontmatter.html',
+    '../jp/Configuration/MarkdownShowcase.html',
+    '../jp/Configuration/edition.html',
+    '../jp/SamplePart.html',
+    '../jp/SamplePart/SampleChapter1.html',
+    '../jp/SamplePart/SampleChapter1/sample1.html',
+    '../jp/SamplePart/SampleChapter1/sample2.html',
+    '../jp/SamplePart/SampleChapter1/サンプル3.html',
+    '../jp/SamplePart/SampleChapter2.html',
+    '../jp/SamplePart/SampleChapter2/sample1.html',
+    '../jp/SamplePart/SampleChapter2/sample2.html',
+    '_pdfcolophon.vivlio.colophon.html'
+  ],
+  output: [
+    'Guide.pdf'
+  ]
+}
