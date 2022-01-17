3a84772f16304697fef600c4d8ee7ea0829397b7
mochimochiki <mochimochiki@users.noreply.github.com>
2022-01-17 14:47:23 +0000

    deploy: 57b7150858874960209cb35f665a3ec7e49b3d82

--- js/menuscroll.js
+++ js/menuscroll.js
@@ -1,9 +1,18 @@
 window.addEventListener("DOMContentLoaded", function () {
-  var menuElement = document.getElementById('menu');
-  var selectedElement = document.getElementById('menu-selected');
-  if (selectedElement) {
-    var yOffset = selectedElement.getBoundingClientRect().top;
+  var pageID = document.head.querySelector("[data-id]")
+  var pageItemElement = document.getElementById(pageID.getAttribute("data-id"));
+  var menuElement = document.getElementById("menu");
+  var detailsElement = pageItemElement.closest("details");
+  while (detailsElement) {
+    detailsElement.open = true;
+    if (detailsElement.parentElement) {
+      detailsElement = detailsElement.parentElement.closest("details");
+    }
+  }
+
+  if (pageItemElement) {
+    var yOffset = pageItemElement.getBoundingClientRect().top;
     var position = yOffset - window.innerHeight / 2;
-    menuElement.scrollTo(0, position);  
+    menuElement.scrollTo(0, position);
   }
 }, false);
