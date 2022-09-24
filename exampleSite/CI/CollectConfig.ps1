0bd7a9c5018e4eb4246462a764c501bebe1f412d
mochimo <mochikimoto@yahoo.co.jp>
2022-09-24 23:35:23 +0900

    configファイル名のhash化廃止、相対パス表示に変更

--- exampleSite/CI/CollectConfig.ps1
+++ exampleSite/CI/CollectConfig.ps1
@@ -11,28 +11,6 @@ Param(
   [String]$path
 )
 
-Function Get-DirHash {
-  [Cmdletbinding()]
-  Param(
-    [Parameter(Mandatory = $true)]
-    [ValidateScript( {
-        if (Test-Path -Path $_ -ErrorAction SilentlyContinue) {
-          return $true
-        }
-        else {
-          throw "$($_) is not a valid path."
-        }
-      })]
-    [string]$Path
-  )
-  $temp = [System.IO.Path]::GetTempFileName()
-  Get-ChildItem -File -Recurse $Path | Get-FileHash | Select-Object -ExpandProperty Hash | Out-File $temp -NoNewline
-  $hash = Get-FileHash $temp
-  Remove-Item $temp
-  $hash.Path = $Path
-  return $hash
-}
-
 function CopyVivlioConfigFile {
   [Cmdletbinding()]
   Param(
@@ -58,25 +36,23 @@ function CopyVivlioConfigFile {
     [string]$OutputPath
   )
   $files = Get-ChildItem -Path $TargetPath | Where-Object { ! $_.PSIsContainer }
-  $hash = Get-DirHash $TargetPath
+  $relativePath = Resolve-Path $TargetPath -Relative
+  $relativePath = $relativePath.TrimStart(".\").Replace("\","_")
   foreach ($file in $files) {
     $tailFileName = Split-Path $file -Leaf
     if ( $tailFileName.Contains('.vivlio.cover.html') ) {
-      $hashValue = $hash.Hash
-      $output = Join-Path $OutputPath "$hashValue.vivlio.cover.html"
+      $output = Join-Path $OutputPath "$relativePath.vivlio.cover.html"
       Copy-Item $file.FullName $output
     }
     if ( $tailFileName.Contains('.vivlio.config.js') ) {
-      $hashValue = $hash.Hash
-      $output = Join-Path $OutputPath "$hashValue.vivlio.config.js"
+      $output = Join-Path $OutputPath "$relativePath.vivlio.config.js"
       Copy-Item $file.FullName $output
-      $data = Get-Content -Encoding utf8 $output | ForEach-Object { $_ -creplace "_pdf.vivlio.cover.html", "$hashValue.vivlio.cover.html" }
-      $data = $data | ForEach-Object { $_ -creplace "_pdfcolophon.vivlio.colophon.html", "$hashValue.vivlio.colophon.html" }
+      $data = Get-Content -Encoding utf8 $output | ForEach-Object { $_ -creplace "_pdf.vivlio.cover.html", "$relativePath.vivlio.cover.html" }
+      $data = $data | ForEach-Object { $_ -creplace "_pdfcolophon.vivlio.colophon.html", "$relativePath.vivlio.colophon.html" }
       $data | Out-File -Encoding utf8 $output | Set-Content -Path $output -Encoding Byte
     }
     if ( $tailFileName.Contains('.vivlio.colophon.html') ) {
-      $hashValue = $hash.Hash
-      $output = Join-Path $OutputPath "$hashValue.vivlio.colophon.html"
+      $output = Join-Path $OutputPath "$relativePath.vivlio.colophon.html"
       Copy-Item $file.FullName $output
     }
   }
@@ -92,6 +68,8 @@ if (-not (Test-Path $path)) {
 }
 
 $dirs = Get-ChildItem -Path $path | Where-Object { $_.PSIsContainer }
+pushd $path
 foreach ($dir in $dirs) {
   CopyVivlioConfigFile $dir.FullName $path
 }
+popd
