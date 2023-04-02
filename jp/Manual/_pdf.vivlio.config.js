2a4443e6bd0b800b9610cea4e0c39d1dd5c828f5
mochimochiki <mochimochiki@users.noreply.github.com>
2023-04-02 13:35:41 +0000

    deploy: 94b18d4e0cbf33d6062ddc348ba011f49bd28c8b

--- jp/Manual/_pdf.vivlio.config.js
+++ jp/Manual/_pdf.vivlio.config.js
@@ -6,14 +6,12 @@ module.exports = {
     '_pdf.vivlio.cover.html',
     
     '../jp/Manual/GettingStarted.html',
+    '../jp/Manual/MarkdownShowcase.html',
+    '../jp/Manual/frontmatter.html',
     '../jp/Manual/config.html',
     '../jp/Manual/pdfconfig.html',
-    '../jp/Manual/shortcodes.html',
-    '../jp/Manual/frontmatter.html',
-    '../jp/Manual/MarkdownShowcase.html',
     '../jp/Manual/edition.html',
     '../jp/Manual/Tags.html',
-    '_pdfcolophon.vivlio.colophon.html'
   ],
   output: [
     'Hugo_Theme_Vivliocli_Manual.pdf'
