2b2a29fa050f1716fc2494e4fab3fb7b0b0e67eb
mochimochiki <mochimochiki@users.noreply.github.com>
2023-04-09 07:37:38 +0000

    deploy: f16ec556d934fe0670b23c9ebb67bf16aea24ddc

---
+++ ja/Manual/_pdf.vivlio.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  title: 'Hugo-theme-vivliocli',
+  author: 'mochimo',
+  size: 'A4',
+  entry: [
+    '_pdf.vivlio.cover.html',
+    
+    '../ja/Manual/GettingStarted.html',
+    '../ja/Manual/MarkdownShowcase.html',
+    '../ja/Manual/frontmatter.html',
+    '../ja/Manual/config.html',
+    '../ja/Manual/pdfconfig.html',
+    '../ja/Manual/edition.html',
+    '../ja/Manual/Tags.html',
+  ],
+  output: [
+    'UserGuide.pdf'
+  ]
+}
---
+++ ja/Test/_pdf.vivlio.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  title: 'Test document',
+  author: 'auther',
+  size: 'A4',
+  entry: [
+    '_pdf.vivlio.cover.html',
+    
+    '../ja/Test/TestChapter1/00_test.html',
+    '../ja/Test/TestChapter1/01_日本語ファイル.html',
+    '../ja/Test/TestChapter1/02_pageTOC.html',
+    '../ja/Test/TestChapter1/03_include.html',
+    '../ja/Test/TestChapter1/04_ShowIfHideIfShortCode.html',
+    '../ja/Test/TestChapter1/05_ShowIfFrontMatter.html',
+    '../ja/Test/TestChapter1/07_TableOfContents.html',
+    '../ja/Test/TestChapter1/08_pagebreak.html',
+    '../ja/Test/TestChapter1/09_ImageResize.html',
+    '../ja/Test/TestChapter1/10_MathJax.html',
+    '../ja/Test/TestChapter1/11_2columns.html',
+    '../ja/Test/TestChapter1/12_2columns_2.html',
+    '../ja/Test/TestChapter1/13_Tags.html',
+  ],
+  output: [
+    'Test.pdf'
+  ]
+}
