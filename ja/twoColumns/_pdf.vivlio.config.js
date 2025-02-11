68bae0bce4ef0089d6754a27938692bcd2aa4639
mochimochiki <mochimochiki@users.noreply.github.com>
2025-02-11 15:30:38 +0000

    deploy: 6b6d5c8f48986c11f81bac29b9fb3edb672aa4eb

---
+++ ja/twoColumns/_pdf.vivlio.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: 'Hugo-theme-vivliocli',
+  author: 'mochimo',
+  size: 'A4',
+  entry: [
+    
+    '../ja/twoColumns/sample.html',
+  ],
+  output: [
+    'TwoColumns.pdf'
+  ]
+}
